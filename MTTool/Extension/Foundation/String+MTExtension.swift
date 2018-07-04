39a2ada185cd409932e1136795d97e6db7d7166f
Nolan <nolan_li@icloud.com>
2018-07-04 17:41:48 +0800

    字符串extension

--- MTTool/Extension/Foundation/String+MTExtension.swift
+++ MTTool/Extension/Foundation/String+MTExtension.swift
@@ -6,30 +6,33 @@
 //  Copyright © 2018年 Nolan. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension String {
-    
-    init ? (base64: String) {
-        let pad = String(repeating: "=", count: base64.length % 4)
-        let base64Padded = base64 + pad
-        if let decodedData = Data(base64Encoded: base64Padded, options: NSData.Base64DecodingOptions(rawValue: 0)), let decodedString = NSString(data: decodedData, encoding: String.Encoding.utf8.rawValue) {
-            self.init(decodedString)
-            return
-        }
-        return nil
-    }
-    
-    /// base64字符串
-    public var base64Str: String {
-        let plainData = (self as NSString).data(using: String.Encoding.utf8.rawValue)
-        let base64String = plainData!.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
-        return base64String
-    }
+
+    /// 转换为NSString类型
+    public var toNSString: NSString { return self as NSString }
     
     /// 字符串长度，同系统属性 count
     public var length: Int {
         return self.count
+    }
+    
+    /// 添加到粘贴板
+    public func addToPasteboard() {
+        let pasteboard = UIPasteboard.general
+        pasteboard.string = self
+    }
+}
+
+// MARK: - 字符相关操作
+extension String {
+    /// 字符串中某个字符的个数
+    ///
+    /// - Parameter substring: 指定字符
+    /// - Returns: 指定字符个数
+    public func count(_ substring: String) -> Int {
+        return components(separatedBy: substring).count - 1
     }
     
     /// 在某个位置的字符
@@ -56,15 +59,70 @@ extension String {
     public subscript(integerClosedRange: ClosedRange<Int>) -> String {
         return self[integerClosedRange.lowerBound..<(integerClosedRange.upperBound + 1)]
     }
+}
+
+// MARK: - 数字型字符串操作
+extension String {
     
-    /// 字符串中某个字符的个数
-    ///
-    /// - Parameter substring: 指定字符
-    /// - Returns: 指定字符个数
-    public func count(_ substring: String) -> Int {
-        return components(separatedBy: substring).count - 1
+    /// 字符串转为NSNumber类型
+    public var number: NSNumber? {
+        return NumberFormatter().number(from: self)
     }
     
+    /// 判断字符串是否是数字型
+    public var isNumber: Bool {
+        return number != nil ? true : false
+    }
+    
+    /// 字符串转为Int类型
+    public var toInt: Int? {
+        return isNumber ? number?.intValue : nil
+    }
+
+    
+    /// 字符串转为Double类型
+    public var toDouble: Double? {
+        return isNumber ? number?.doubleValue : nil
+    }
+    
+    /// 字符串转为Float类型
+    public var toFloat: Float? {
+        return isNumber ? number?.floatValue : nil
+    }
+    
+    /// EZSE: Converts String to Bool
+//    public func toBool() -> Bool? {
+//        let trimmedString = trimmed().lowercased()
+//        if trimmedString == "true" || trimmedString == "false" {
+//            return (trimmedString as NSString).boolValue
+//        }
+//        return nil
+//    }
+
+}
+
+// MARK: - 64位字符串
+extension String {
+    init ? (base64: String) {
+        let pad = String(repeating: "=", count: base64.length % 4)
+        let base64Padded = base64 + pad
+        if let decodedData = Data(base64Encoded: base64Padded, options: NSData.Base64DecodingOptions(rawValue: 0)), let decodedString = NSString(data: decodedData, encoding: String.Encoding.utf8.rawValue) {
+            self.init(decodedString)
+            return
+        }
+        return nil
+    }
+    
+    /// base64字符串
+    public var base64Str: String {
+        let plainData = (self as NSString).data(using: String.Encoding.utf8.rawValue)
+        let base64String = plainData!.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
+        return base64String
+    }
+}
+
+// MARK: - 字符串类型判断
+extension String {
     /// 字符串中是否包含表情
     ///
     /// - Returns: true：包含，false: 不包含
