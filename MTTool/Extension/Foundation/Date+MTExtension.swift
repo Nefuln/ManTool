b54b8d9b1c475c41d53057e0b3db2dcaa3518042
Nolan <nolan_li@icloud.com>
2018-09-13 17:47:28 +0800

    add

--- MTTool/Extension/Foundation/Date+MTExtension.swift
+++ MTTool/Extension/Foundation/Date+MTExtension.swift
@@ -7,36 +7,50 @@
 import Foundation
 
 extension Date {
+    
+    /// 年份
     public var year: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.year, from: self)
     }
     
+    /// 季度
+    public var quarter: Int {
+        let currentCalendar = Calendar.current
+        return currentCalendar.component(Calendar.Component.quarter, from: self)
+    }
+    
+    /// 月份
     public var month: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.month, from: self)
     }
     
+    /// 日期
     public var day: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.day, from: self)
     }
     
+    /// 小时
     public var hour: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.hour, from: self)
     }
     
+    /// 分钟
     public var minute: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.minute, from: self)
     }
     
+    /// 秒
     public var second: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.second, from: self)
     }
     
+    /// 周几
     public var weekday: Int {
         let currentCalendar = Calendar.current
         return currentCalendar.component(Calendar.Component.weekday, from: self)
